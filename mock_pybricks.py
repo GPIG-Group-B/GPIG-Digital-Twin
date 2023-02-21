ea7596411ec59409490592d9212328fa1b4f38da
Nebulised <dhenleymarshall@gmail.com>
2023-02-21 16:01:31 +0000

    Created abstract device class

--- mock_pybricks.py
+++ mock_pybricks.py
@@ -14,18 +14,38 @@ class Direction:
     COUNTER_CLOCKWISE = "counter_clockwise"
     CLOCKWISE = "clockwise"
 
+class PybricksDevice:
 
-class Motor:
-
-    def __init__(self,
-                 port : dict,
-                 positive_direction : Direction,
-                 gears : list,
-                 reset_angle : bool):
-        self._DEVICE_TYPE_ID = 0
+    def __init__(self, port, device_type_id):
         self._port, self._additional_data = setup_server_connction(**port,
                                                                    num_connections=1)
+        self._DEVICE_TYPE_ID = device_type_id
         send_device_type_id(connection=self._port, device_type_id=self._DEVICE_TYPE_ID)
+
+    def _send_info_message(self):
+        MESSAGE_ID = 0
+        EXCLUDE_FROM_INFO_MESSAGE = ["_port", "_additional_data"]
+        self.send_message(data=vars(self),
+                          exclusions=EXCLUDE_FROM_INFO_MESSAGE,
+                          message_id=MESSAGE_ID)
+
+    def send_message(self, data : dict, message_id : int, exclusions = None):
+        data = {key.strip("_") : val for key,val in data.items() if key not in exclusions}
+        send_json_message(connection=self._port,
+                          message_dict=data,
+                          message_id=message_id)
+
+
+class Motor(PybricksDevice):
+
+    def __init__(self,
+                 port: dict,
+                 positive_direction: Direction,
+                 gears: list,
+                 reset_angle: bool):
+        super().__init__(port,
+                         device_type_id=0)
+        self._DEVICE_TYPE_ID = 0
         self._positive_direction = positive_direction
         self._gears = gears
         self._reset_angle = reset_angle
@@ -33,14 +53,6 @@ class Motor:
         self._angle = 0
         self._load = "unknown"
         self._send_info_message()
-
-
-    def _send_info_message(self):
-        EXCLUDE_FROM_INFO_MESSAGE = ["port", "additional_data"]
-        send_json_message(connection=self._port,
-                          message_dict={key.strip("_") : val for key,val in vars(self).items() if not any(exclusion in key for exclusion in EXCLUDE_FROM_INFO_MESSAGE)},
-                          message_id=self._DEVICE_TYPE_ID)
-
 
     def speed(self):
         return self._speed
@@ -52,45 +64,39 @@ class Motor:
         self._angle = angle
 
     def stop(self):
-        MESSAGE_ID = 0
-        send_json_message(connection=self._port,
-                          message_dict={},
+        MESSAGE_ID = 1
+        self.send_message(data={},
                           message_id=MESSAGE_ID)
 
+
     def brake(self):
-        MESSAGE_ID = 1
-        send_json_message(connection=self._port,
-                          message_dict={},
+        MESSAGE_ID = 2
+        self.send_message(data={},
                           message_id=MESSAGE_ID)
     def hold(self):
-        MESSAGE_ID = 2
-        send_json_message(connection=self._port,
-                          message_dict={},
+        MESSAGE_ID = 3
+        self.send_message(data={},
                           message_id=MESSAGE_ID)
 
     def run(self, speed : int):
         MESSAGE_ID = 4
-        send_json_message(connection=self._port,
-                          message_dict={"speed" : speed},
+        self.send_message(data=locals(),
+                          exclusions=["self"],
                           message_id=MESSAGE_ID)
 
     def run_time(self, speed : int, time : int, then = None, wait = True):
         MESSAGE_ID = 5
-        send_json_message(connection=self._port,
-                          message_dict={"speed" : speed,
-                                        "time" : time,
-                                        "then" : then,
-                                        "wait" : wait},
+        self.send_message(data=locals(),
+                          exclusions=["self"],
                           message_id=MESSAGE_ID)
 
     def run_angle(self, speed : int, rotation_angle : int, then = None, wait=True):
         MESSAGE_ID = 6
-        send_json_message(connection=self._port,
-                          message_dict={"speed": speed,
-                                        "rotation_angle": rotation_angle,
-                                        "then": then,
-                                        "wait": wait},
+        self.send_message(data=locals(),
+                          exclusions=["self"],
                           message_id=MESSAGE_ID)
+
+
 
 
 
