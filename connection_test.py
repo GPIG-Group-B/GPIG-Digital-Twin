7d7df731db985de65b8033cb6c9a436e8e43db79
Nebulised <dhenleymarshall@gmail.com>
2023-02-21 15:27:25 +0000

    Converted into C# <-> Python test script

--- connection_test.py
+++ connection_test.py
@@ -1,5 +1,11 @@
 from connections import TCPWorker
+from mock_pybricks import Motor, Port, Direction
 
 test_worker = TCPWorker()
-test_worker.run()
+test_worker.start()
+test_motor = Motor(port = Port.A,
+                   positive_direction=Direction.CLOCKWISE,
+                   gears=[],
+                   reset_angle=False)
+test_motor.run(4)
 test_worker.join()
