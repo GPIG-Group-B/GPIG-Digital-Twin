fd2787baf843455e4b6e1abd93bc1e4ffdf5ecb6
Nebulised <dhenleymarshall@gmail.com>
2023-02-21 15:25:33 +0000

    Created TCPWorker to emulate C# TCP Worker

--- connections.py
+++ connections.py
@@ -1,87 +1,54 @@
-import multiprocessing
 import socket
 import threading
 import multiprocessing
+import time
 
+from connection_utils import receive_data
 
-def receive_data(connection, length, data=b""):
-    while len(data) < length:
-        data += connection.recv(length - len(data))
-        if not data:
-            raise Exception("Connection was closed")
-    return data[:length], data[length:]
 
 class TCPWorker(multiprocessing.Process):
 
     def __init__(self, ip="localhost", port=65432):
-        super().__init__()
         self._ip = ip
         self._port = port
-
-
         self._close_connection_flag = multiprocessing.Event()
-
-    def run(self):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind((self._ip, self._port))
-            print(f"Waiting for connection on IP : {self._ip} | PORT : {self._port}")
-            s.listen(1)
-            connection, _ = s.accept()
-            print(f"Accepted connection on IP : {self._ip} | PORT : {self._port}")
-
-        sender_worker = TCPSenderWorker(connection=connection, connection_close_flag=self._close_connection_flag)
-        receiver_worker = TCPReceiverWorker(connection=connection, connection_close_flag=self._close_connection_flag)
-        sender_worker.run()
-        receiver_worker.run()
-        sender_worker.join()
-        receiver_worker.join()
-        connection.shutdown(socket.SHUT_RDWR)
-        connection.close()
-
-
-
-
-
-
-
-
-
-
-class TCPSenderWorker(threading.Thread):
-
-    def __init__(self, connection : socket.socket, connection_close_flag):
+        self._additional_data = b""
+        self.connection = True
         super().__init__()
-        self._connection = connection
-        self._connection_close_flag = connection_close_flag
-        print("Initialised TCP Sender Worker")
 
     def run(self):
-        n = 0
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print(f"Connecting using IP : {self._ip} | PORT : {self._port}")
         while True:
-            n += 1
-            if self._connection_close_flag.is_set():
-                print("Connection closed")
+            try:
+                self.connection.connect((self._ip, self._port))
                 break
-            self._connection.sendall(str.encode(str(n)))
+            except:
+                print("Connection refused.  Trying again in 3 seconds")
+                time.sleep(3)
+        print(f"Accepted connection on IP : {self._ip} | PORT : {self._port}")
+        self.connection.sendall("GPIG-Group-B".encode())
 
+        self._device_type_id, self._additional_data = receive_data(connection=self.connection,
+                                                                   length=2,
+                                                                   data=self._additional_data)
+        self._device_type_id = int.from_bytes(self._device_type_id, byteorder="big")
 
-class TCPReceiverWorker(threading.Thread):
-
-    def __init__(self, connection : socket.socket, connection_close_flag : multiprocessing.Event):
-        super().__init__()
-        self._connection = connection
-        self._connection_close_flag = connection_close_flag
-        print("Initialised TCP Receiver Worker")
-
-    def run(self):
-        existing_data = b""
         while True:
-            data_received, existing_data = receive_data(self._connection, length=1024, data=existing_data)
-            print(data_received)
-            if data_received == str.encode("E"):
-                self._connection_close_flag.set()
-                break
+            self._receive_json()
 
 
-
+    def _receive_json(self):
+        message_type, self._additional_data = receive_data(connection=self.connection,
+                                                         length=2,
+                                                         data=self._additional_data)
+        print(f"Message type : {int.from_bytes(message_type, byteorder='big')}")
+        message_length, self._additional_data = receive_data(connection=self.connection,
+                                                             length=2,
+                                                             data=self._additional_data)
+        message_length = int.from_bytes(message_length, byteorder="big")
+        json_data, self._additional_data = receive_data(connection=self.connection,
+                                                        length=message_length,
+                                                        data=self._additional_data)
+        print(json_data)
 
