5ee3fe752e3cbea4f12a18af2d5fb319a4b95d25
Fred Dodd <fpd500@york.ac.uk>
2023-03-01 17:52:56 +0000

    connection changes

--- connection_test.py
+++ connection_test.py
@@ -1,11 +1,14 @@
-from connections import TCPWorker
-from mock_pybricks import Motor, Port, Direction
+def main():
+    from connections import TCPWorker
+    from mock_pybricks import Motor, Port, Direction
+    # test_worker = TCPWorker()
+    # test_worker.start()
+    test_motor = Motor(port = Port.A,
+                    positive_direction=Direction.CLOCKWISE,
+                    gears=[],
+                    reset_angle=False)
+    test_motor.run(4)
+    # test_worker.join()
 
-test_worker = TCPWorker()
-test_worker.start()
-test_motor = Motor(port = Port.A,
-                   positive_direction=Direction.CLOCKWISE,
-                   gears=[],
-                   reset_angle=False)
-test_motor.run(4)
-test_worker.join()
+if __name__ == "__main__":
+    main()
