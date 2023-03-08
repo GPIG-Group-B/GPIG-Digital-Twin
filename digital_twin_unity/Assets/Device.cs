1a602a4ab3905bbcc1a79e00a11f42980ae4a56f
Dylan <dhenleymarshall@gmail.com>
2023-03-08 14:01:37 +0000

    Implemented run method for constantly receiving messages Moved checking of device id to device class

--- digital_twin_unity/Assets/Device.cs
+++ digital_twin_unity/Assets/Device.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using System.Net.Sockets;
 using System;
 using System.Linq;
+using static UnityEngine.Networking.UnityWebRequest;
+using System.Security.Cryptography;
 
 public abstract class Device
 { 
@@ -12,22 +14,33 @@ public abstract class Device
 
 
     protected int port;
-    protected int device_id;
+    protected int deviceID;
     protected string ip;
+    protected SocketHandler socketHandler;
+    protected Dictionary<Int16, Action<string>> message_dict = new Dictionary<Int16, Action<string>>();
 
 
 
-    public Device(string ip,int port,int device_id)
+    public Device(string ip,int port,int deviceID)
     {
         this.ip = ip;
         this.port = port;
-        this.device_id = device_id;
-        this.Socket = new SocketHandler(ip: this.ip, port: this.port);
-        Int16 received_device_id = this.Socket.setupSocket();
+        this.deviceID = deviceID;
+        this.socketHandler = new SocketHandler(ip: this.ip, port: this.port);
+        // This must be called after socket setup
+        this.CheckDeviceID();
+
+
         // check right device id
-        if (received_device_id != device_id)
+  
+    }
+
+    private void CheckDeviceID() 
+    {
+        Int16 received_device_id = this.socketHandler.GetHeaderData();
+        if (received_device_id != deviceID)
         {
-            throw new Exception("receive device id: "+ received_device_id + ". Expected device_id:" + device_id);
+            throw new Exception("receive device id: " + received_device_id + ". Expected deviceID:" + deviceID);
 
         }
         else
@@ -38,15 +51,19 @@ public abstract class Device
 
     public void run()
     {
+        Int16 message_id;
+        string message_json_string;
+
+        
+
 
         while (true)
         {
-            var device_message_data =Socket.get_device_message();
+            var returned_socket_data = Socket.get_device_message();
+            message_id = returned_socket_data.Item1;
+            message_json_string = returned_socket_data.Item2;
 
-            /*
-            this.get_command(device_message_data.message_id);
-            this.command(device_message_data.device_message);
-            */
+            this.message_dict[message_id](message_json_string);
 
 
 
@@ -54,6 +71,9 @@ public abstract class Device
 
 
     }
+
+
+
 
 
 
