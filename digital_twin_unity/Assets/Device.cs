8b301be314a8f53f7007c13d4457fde59088f723
Nebulised <dhenleymarshall@gmail.com>
2023-04-05 12:34:12 +0100

    Updated device to use monobehaviour Now uses inbound and outbound thread safe message queues to run methods in main unity thread

--- digital_twin_unity/Assets/Device.cs
+++ digital_twin_unity/Assets/Device.cs
@@ -6,33 +6,42 @@ using System;
 using System.Linq;
 using static UnityEngine.Networking.UnityWebRequest;
 using System.Security.Cryptography;
+using System.Threading;
+using System.Runtime.CompilerServices;
+using System.Collections.Concurrent;
 
-public abstract class Device
+public abstract class Device : MonoBehaviour
 { 
-    protected int port;
+    public int port;
+    public string ip;
     protected int deviceID;
-    protected string ip;
     protected SocketHandler socketHandler;
-    protected Dictionary<Int16, Func<string, string>> message_dict;
+    protected Dictionary<Int16, Func<string, string>> message_dict = new Dictionary<Int16, Func<string, string>>();
+    protected BlockingCollection<Message> inboundMessageQueue = new BlockingCollection<Message>(new ConcurrentQueue<Message>());
+    protected BlockingCollection<Message> outboundMessageQueue = new BlockingCollection<Message>(new ConcurrentQueue<Message>());
+    private Thread socketThread;
 
 
-
-    public Device(string ip,int port,int deviceID)
+    protected virtual void Start() 
     {
-        this.ip = ip;
-        this.port = port;
-        this.deviceID = deviceID;
-        this.socketHandler = null;
-        this.message_dict = new Dictionary<Int16, Func<string, string>>();
-        // IMPORTANT - 0 is reserved for closing the connection
-        // IMPORTANT - 1 is reserved for info message
-        // All custom device methods must be assigned to key 2 or higher (excl info which must be 1) 
-
-        // check right device id
-
+        socketThread = new Thread(new ThreadStart(this.run));
+        socketThread.Start();
     }
 
-    private void CheckDeviceID() 
+    protected virtual void Update() 
+    {
+        Message inboundMessage;
+        if (inboundMessageQueue.TryTake(out inboundMessage)) 
+        {
+            string returnMessage = message_dict[inboundMessage.messageID](inboundMessage.jsonString);
+            Message outboundMessage = new Message();
+            outboundMessage.jsonString = returnMessage;
+            outboundMessage.messageID = inboundMessage.messageID;
+            outboundMessageQueue.Add(outboundMessage);
+        }
+    }
+
+    protected virtual void CheckDeviceID() 
     {
         Int16 received_device_id = this.socketHandler.GetHeaderData();
         if (received_device_id != deviceID)
@@ -46,37 +55,62 @@ public abstract class Device
         }
     }
 
-    public void run()
+    protected virtual void run()
     {
+        Debug.Log("Initialising Connection on IP : " + this.ip + " | PORT : " + this.port);
         this.socketHandler = new SocketHandler(ip: this.ip, port: this.port);
+        Debug.Log("Connection established. Checking device ID");
         // This must be called after socket setup
         this.CheckDeviceID();
+        Debug.Log("Device ID Check successful");
 
-        Int16 message_id;
-        string message_json_string;
 
-        
+        Message outboundMessage;
+
+
 
 
         while (true)    
         {
+            Message inboundMessage = new Message();
             var returned_socket_data = this.socketHandler.get_device_message();
-            message_id = returned_socket_data.Item1;
-            message_json_string = returned_socket_data.Item2;
-            if (message_id == 0)
+            inboundMessage.messageID = returned_socket_data.Item1;
+            inboundMessage.jsonString = returned_socket_data.Item2;
+            if (inboundMessage.messageID == 0)
             {
                 Debug.Log("Received shutdown message. Closing TCP Worker");
                 break;
             }
 
-            string returnMessageJson = this.message_dict[message_id](message_json_string);
-            this.socketHandler.SendJSONMessage(messageID : message_id, jsonMessage : returnMessageJson);
+            inboundMessageQueue.Add(inboundMessage);
+            outboundMessage = outboundMessageQueue.Take();
+            if (outboundMessage.messageID != inboundMessage.messageID) 
+            {
+                throw new Exception("Message IDs do not match. Something has gone very very wrong :).");
+            }
+            // string returnMessageJson = this.message_dict[message_id](message_json_string);
+
+            this.socketHandler.SendJSONMessage(messageID : outboundMessage.messageID, jsonMessage : outboundMessage.jsonString);
 
 
 
         }
 
+       
 
+    }
+
+    void OnDestroy() 
+    {
+        Debug.Log("Waiting for socket thread to finish");
+        socketThread.Join();
+        Debug.Log("Socket thread joined successfully");
+    }
+
+    protected struct Message 
+    {
+        public Int16 messageID;
+        public string jsonString;
     }
 
 
