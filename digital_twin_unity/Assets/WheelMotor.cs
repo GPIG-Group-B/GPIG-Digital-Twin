af59189b7d68793daa3afedd864231d49db4a20e
Dylan <dhenleymarshall@gmail.com>
2023-04-22 17:51:01 +0100

    Renamed Motor to WheelMotor

--- digital_twin_unity/Assets/WheelMotor.cs
+++ digital_twin_unity/Assets/WheelMotor.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Motor : Device
+public class WheelMotor : Device
 {
     private static Int16 DEVICE_ID = 0;
     private string _positiveDirection;
