01a6de9c1f4f574e1cac381378cafe514d545486
Dylan <dhenleymarshall@gmail.com>
2023-05-03 14:54:56 +0100

    Updated constants for colour, colour distance and force sensor

--- constants.py
+++ constants.py
@@ -7,20 +7,34 @@ AXLE_TRACK = 121
 # Maximum angle the rover can turn in degrees
 MAX_TURN_ANGLE = 22
 
+
+
+#### POWERED UP HUB
+
 # Port assignment letter for left motor
-LEFT_MOTOR_PORT = "E"
+LEFT_MOTOR_PORT = "A"
 
 # Port assignment letter for right motor
-RIGHT_MOTOR_PORT = "F"
+RIGHT_MOTOR_PORT = "B"
 
 # Port assignment letter for steering motor
 STEERING_MOTOR_PORT = "C"
+
+
+### LEGO SPIKE HUB
+
 
 # Port assignment letter for ultrasonic sensor
 ULTRASONIC_SENSOR_PORT = "A"
 
 # Port assignment letter for ultrasonic motor sensor
 ULTRASONIC_MOTOR_PORT = "B"
+
+COLOUR_SENSOR_PORT = "C"
+
+FORCE_SENSOR_PORT = "D"
+
+COLOUR_DISTANCE_SENSOR_PORT = "E"
 
 # Degrees scan start
 SCAN_START = -120
