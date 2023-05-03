9a434f88860a2df020360a35666d3f861c1a9b72
Dylan <dhenleymarshall@gmail.com>
2023-05-03 14:54:41 +0100

    Implemented seperate spike and powered up hub

--- utils.py
+++ utils.py
@@ -1,29 +1,62 @@
-try:
-    from pybricks.parameters import Port
-except:
-    from mock_pybricks import Port
+class PortIPStruct:
+
+    def __init__(self, ip: str, port: int):
+        self.ip = ip
+        self.port = port
+
+class LegoSpikeHub:
+
+    A = "who knows"
+
+    def __init__(self):
+        try:
+            from pybricks.parameters import Port
+            self.port_a = Port.A
+            self.port_b = Port.B
+            self.port_c = Port.C
+            self.port_d = Port.D
+            self.port_e = Port.E
+            self.port_f = Port.F
+        except ImportError:
+            self.port_a = PortIPStruct(ip = "localhost", port = 65490)
+            self.port_b = PortIPStruct(ip="localhost", port=65491)
+            self.port_c = PortIPStruct(ip="localhost", port=65492)
+            self.port_d = PortIPStruct(ip="localhost", port=65493)
+            self.port_e = PortIPStruct(ip="localhost", port=65494)
+            self.port_f = PortIPStruct(ip="localhost", port=65495)
+
+        self.str_conversion_dict = {"A" : self.port_a,
+                                    "B" : self.port_b,
+                                    "C" : self.port_c,
+                                    "D" : self.port_d,
+                                    "E" : self.port_e,
+                                    "F" : self.port_f}
+
+    def get_port_from_str(self, port_as_string):
+        return self.str_conversion_dict[port_as_string.upper()]
 
 
+class PoweredUpHub:
 
-def convert_str_to_port(
-        port_as_string: str):
-    """Utility method for getting the pybricks Port object from a string
 
-    Args:
-        port_as_string:
-            The port letter as a string to convert
+    def __init__(self):
+        try:
+            from pybricks.parameters import Port
+            self.port_a = Port.A
+            self.port_b = Port.B
+            self.port_c = Port.C
+            self.port_d = Port.D
+        except:
+            self.port_a = PortIPStruct(ip = "localhost", port = 65496)
+            self.port_b = PortIPStruct(ip="localhost", port=65497)
+            self.port_c = PortIPStruct(ip="localhost", port=65498)
+            self.port_d = PortIPStruct(ip="localhost", port=65499)
 
-    Returns:
-        Pybricks Port object
+        self.str_conversion_dict = {"A" : self.port_a,
+                                    "B" : self.port_b,
+                                    "C" : self.port_c,
+                                    "D" : self.port_d}
 
-    """
-    conversion_dict = {"A": Port.A,
-                       "B": Port.B,
-                       "C": Port.C,
-                       "D": Port.D,
-                       "E": Port.E,
-                       "F": Port.F}
-    if port_as_string not in conversion_dict:
-        raise ValueError(f"Port identifier string must be one of {conversion_dict.keys()}")
+    def get_port_from_str(self, port_as_string):
+        return self.str_conversion_dict[port_as_string.upper()]
 
-    return conversion_dict[port_as_string]
