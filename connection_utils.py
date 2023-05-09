eb72a0ba5cb1da2b2b9ebf39424163308d8a98eb
Nebulised <dhenleymarshall@gmail.com>
2023-05-09 17:20:10 +0100

    Removed redundant param

--- connection_utils.py
+++ connection_utils.py
@@ -31,6 +31,26 @@ def setup_server_connction(ip,
             connection.sendall(ACKNOWLEDGEMENT_CONNECTION_STRING)
     return connection, additional_data
 
+def setup_client_connection(ip,
+                           port):
+    with socket.socket(socket.AF_INET,
+                       socket.SOCK_STREAM) as s:
+        try:
+            s.connect((ip, port))
+        except OSError as e:
+            print(f"Got error {e}. Retrying in 3 seconds")
+            time.sleep(3)
+        print(f"Accepted connection on IP : {ip} | PORT : {port}")
+        s.sendall(INIT_CONNECTION_STRING)
+        data, additional_data = receive_data(connection=s,
+                                             length=len(ACKNOWLEDGEMENT_CONNECTION_STRING))
+        if data != INIT_CONNECTION_STRING:
+            print(f"Expected init string : {ACKNOWLEDGEMENT_CONNECTION_STRING}. Received {data} Closing connection")
+            s.close()
+        else:
+            print("Received expected init string. Accepting connection. Sending confirmation string")
+    return s, additional_data
+
 
 def send_json_message(connection: socket.socket,
                       message_dict: dict,
