80bb5bcd85d3a6acc04423a4b9dda67940129fa9
Lucy Ivatt <lucyivatt@gmail.com>
2023-05-17 19:14:55 +0100

    added gears to steering base, commented out all test cases, still need moving to separate file

--- distance_test.py
+++ distance_test.py
@@ -17,7 +17,7 @@ R_MOTOR = Motor(port=HUB.get_port_from_str(constants.RIGHT_MOTOR_PORT),
                         positive_direction=Direction.CLOCKWISE)
 
 STEERING_MOTOR = Motor(port=HUB.get_port_from_str(constants.STEERING_MOTOR_PORT),
-                               positive_direction=Direction.COUNTERCLOCKWISE)
+                               positive_direction=Direction.COUNTERCLOCKWISE, gears=[12, 36])
 
 DRIVE_BASE = DriveBase(left_motor=L_MOTOR, right_motor=R_MOTOR, wheel_diameter=constants.WHEEL_DIAMETER,
                         axle_track=constants.AXLE_TRACK)
@@ -29,8 +29,8 @@ def move_straight(distance, forward=True):
         DRIVE_BASE.straight(-distance, wait=True)
 
 def drive_at_curve(angle, distance):
-    if abs(angle) > constants.MAX_TURN_ANGLE:
-        raise ValueError(f"Provided angle {angle} must be less than the max turn angle : {constants.MAX_TURN_ANGLE}")
+    # if abs(angle) > constants.MAX_TURN_ANGLE:
+    #     raise ValueError(f"Provided angle {angle} must be less than the max turn angle : {constants.MAX_TURN_ANGLE}")
 
     DEFAULT_SPEED = 100
 
@@ -44,84 +44,84 @@ def drive_at_curve(angle, distance):
         DRIVE_BASE.curve(radius=rad, angle=arc)
 
 def main():
-    # Forward Tests
-    move_straight(1000) # 1m
-    move_straight(500) # 50cm
-    move_straight(250) # 25cm
-    move_straight(200) # 20cm
-    move_straight(100) # 10cm
+    # # Forward Tests
+    # move_straight(1000) # 1m
+    # move_straight(500) # 50cm
+    # move_straight(250) # 25cm
+    # move_straight(200) # 20cm
+    # move_straight(100) # 10cm
 
-    # Backwards Tests
-    move_straight(1000, forward=False) # 1m
-    move_straight(500, forward=False) # 50cm
-    move_straight(250, forward=False) # 25cm
-    move_straight(200, forward=False) # 20cm
-    move_straight(100, forward=False) # 10cm
+    # # Backwards Tests
+    # move_straight(1000, forward=False) # 1m
+    # move_straight(500, forward=False) # 50cm
+    # move_straight(250, forward=False) # 25cm
+    # move_straight(200, forward=False) # 20cm
+    # move_straight(100, forward=False) # 10cm
 
-    # Curve Tests
-    ## 5 degrees left forward
-    drive_at_curve(-5, 500) # 50cm
-    drive_at_curve(-5, 250) # 25cm
-    drive_at_curve(-5, 100) # 10cm
+    # # Curve Tests
+    # ## 5 degrees left forward
+    # drive_at_curve(-5, 500) # 50cm
+    # drive_at_curve(-5, 250) # 25cm
+    # drive_at_curve(-5, 100) # 10cm
 
-    # 5 degrees right forward
-    drive_at_curve(5, 500) # 50cm
-    drive_at_curve(5, 250) # 25cm
-    drive_at_curve(5, 100) # 10cm
+    # # 5 degrees right forward
+    # drive_at_curve(5, 500) # 50cm
+    # drive_at_curve(5, 250) # 25cm
+    # drive_at_curve(5, 100) # 10cm
 
-    ## 5 degrees left backwards
-    drive_at_curve(-5, -500) # 50cm
-    drive_at_curve(-5, -250) # 25cm
-    drive_at_curve(-5, -100) # 10cm
+    # ## 5 degrees left backwards
+    # drive_at_curve(-5, -500) # 50cm
+    # drive_at_curve(-5, -250) # 25cm
+    # drive_at_curve(-5, -100) # 10cm
 
-    # 5 degrees right backwards
-    drive_at_curve(5, -500) # 50cm
-    drive_at_curve(5, -250) # 25cm
-    drive_at_curve(5, -100) # 10cm
+    # # 5 degrees right backwards
+    # drive_at_curve(5, -500) # 50cm
+    # drive_at_curve(5, -250) # 25cm
+    # drive_at_curve(5, -100) # 10cm
 
-    # ----------------------------
+    # # ----------------------------
 
-    ## 10 degrees left forward
-    drive_at_curve(-10, 500) # 50cm
-    drive_at_curve(-10, 250) # 25cm
-    drive_at_curve(-10, 100) # 10cm
+    # ## 10 degrees left forward
+    # drive_at_curve(-10, 500) # 50cm
+    # drive_at_curve(-10, 250) # 25cm
+    # drive_at_curve(-10, 100) # 10cm
 
-    # 10 degrees right forward
-    drive_at_curve(10, 500) # 50cm
-    drive_at_curve(10, 250) # 25cm
-    drive_at_curve(10, 100) # 10cm
+    # # 10 degrees right forward
+    # drive_at_curve(10, 500) # 50cm
+    # drive_at_curve(10, 250) # 25cm
+    # drive_at_curve(10, 100) # 10cm
 
-    ## 10 degrees left backwards
-    drive_at_curve(-10, -500) # 50cm
-    drive_at_curve(-10, -250) # 25cm
-    drive_at_curve(-10, -100) # 10cm
+    # ## 10 degrees left backwards
+    # drive_at_curve(-10, -500) # 50cm
+    # drive_at_curve(-10, -250) # 25cm
+    # drive_at_curve(-10, -100) # 10cm
 
-    # 10 degrees right backwards
-    drive_at_curve(10, -500) # 50cm
-    drive_at_curve(10, -250) # 25cm
-    drive_at_curve(10, -100) # 10cm
+    # # 10 degrees right backwards
+    # drive_at_curve(10, -500) # 50cm
+    # drive_at_curve(10, -250) # 25cm
+    # drive_at_curve(10, -100) # 10cm
 
-    # ----------------------------
-    ## 15 degrees left forward
-    drive_at_curve(-15, 500) # 50cm
-    drive_at_curve(-15, 250) # 25cm
-    drive_at_curve(-15, 100) # 10cm
+    # # ----------------------------
+    # ## 15 degrees left forward
+    # drive_at_curve(-15, 500) # 50cm
+    # drive_at_curve(-15, 250) # 25cm
+    # drive_at_curve(-15, 100) # 10cm
 
-    # 15 degrees right forward
-    drive_at_curve(15, 500) # 50cm
-    drive_at_curve(15, 250) # 25cm
-    drive_at_curve(15, 100) # 10cm
+    # # 15 degrees right forward
+    # drive_at_curve(15, 500) # 50cm
+    # drive_at_curve(15, 250) # 25cm
+    # drive_at_curve(-15, 20) # 10cm
 
-    ## 15 degrees left backwards
-    drive_at_curve(-15, -500) # 50cm
-    drive_at_curve(-15, -250) # 25cm
-    drive_at_curve(-15, -100) # 10cm
+    # ## 15 degrees left backwards
+    # drive_at_curve(-15, -500) # 50cm
+    # drive_at_curve(-15, -250) # 25cm
+    # drive_at_curve(-15, -100) # 10cm
 
-    # 15 degrees right backwards
-    drive_at_curve(15, -500) # 50cm
-    drive_at_curve(15, -250) # 25cm
-    drive_at_curve(15, -100) # 10cm
+    # # 15 degrees right backwards
+    # drive_at_curve(15, -500) # 50cm
+    # drive_at_curve(15, -250) # 25cm
+    # drive_at_curve(15, -100) # 10cm
+
+    STEERING_MOTOR.run_target(speed=100, target_angle=-15)
 
 main()
-
-print("hello")
